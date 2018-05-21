c11b627752f0abfa0b7eab85a26d08059cfa6c3d
Igor Grafin <apaticmail@gmail.com>
2018-05-21 23:20:31 +0300

    initial

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,33 @@
+import requests
+import whois
+import sys
+
+
 def load_urls4check(path):
-    pass
+    print(path)
+    with open(path, mode="r") as file:
+        return file.readlines()
+
 
 def is_server_respond_with_200(url):
-    pass
+    try:
+        response = requests.get(url, allow_redirects=True)
+    except requests.exceptions.ConnectionError:
+        return "Warning! No response!"
+    return "Status - OK"
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain_info = whois.whois(domain_name)
+    if domain_info["expiration_date"]:
+        return domain_info["expiration_date"][0]
 
-if __name__ == '__main__':
-    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit("Put a file as a parameter")
+    sites = load_urls4check(sys.argv[1])
+    for site in sites:
+        print(site)
+        print(is_server_respond_with_200(site))
+        print(get_domain_expiration_date(site))
