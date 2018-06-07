c473bbe4c528fc42dfd65323b816ac555a7bbef9
Igor Grafin <apaticmail@gmail.com>
2018-06-07 22:34:04 +0300

    initial

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Sites Monitoring Utility
+This script will help you to monitor web-sites. It shows actual status code and days to expiration of the domain.  
+## Installation
+Install requirements.
+```pip install -r requirements.txt```
 
-[TODO. There will be project description]
+## How to use
+Put a text file which contains web-sites' ulrs you want to monitor as a parameter. Each url on a new line.
+See the example below. 
+```python.exe E:/GitHub/devman/17_sites_monitoring/check_sites_health.py sites.txt
+1) https://devman.org/
+Status code - 200
+Days to expiration - 81
+2) https://www.youtube.com/
+Status code - 200
+Days to expiration - 252
+3) https://www.rbc8.ru/
+Status code - No response!
+4) https://rbc.ru
+Status code - 200
+Days to expiration - 53
+```
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -1,33 +1,48 @@
 import requests
 import whois
 import sys
+import datetime
 
 
 def load_urls4check(path):
-    print(path)
     with open(path, mode="r") as file:
         return file.readlines()
 
 
 def is_server_respond_with_200(url):
     try:
-        response = requests.get(url, allow_redirects=True)
+        response = requests.get(url)
+        return response.status_code
     except requests.exceptions.ConnectionError:
-        return "Warning! No response!"
-    return "Status - OK"
+        return "No response!"
 
 
 def get_domain_expiration_date(domain_name):
     domain_info = whois.whois(domain_name)
-    if domain_info["expiration_date"]:
-        return domain_info["expiration_date"][0]
+    expiration_date = domain_info["expiration_date"]
+    if type(expiration_date) == list:
+        return expiration_date[0]
+    return expiration_date
+
+
+def get_days_to_expiration(expiration_date):
+    now = datetime.datetime.now()
+    delta = expiration_date - now
+    return str(delta.days)
+
+
+def print_checking_results(index, site):
+    print("{}) {}".format(index, site))
+    print("Status code - {}".format(is_server_respond_with_200(site)))
+    expiration_date = get_domain_expiration_date(site)
+    if expiration_date:
+        print("Days to expiration - {}".format(get_days_to_expiration(expiration_date)))
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        exit("Put a file as a parameter")
+        exit("Put a file path as an argument")
     sites = load_urls4check(sys.argv[1])
-    for site in sites:
-        print(site)
-        print(is_server_respond_with_200(site))
-        print(get_domain_expiration_date(site))
+    for index, site in enumerate(sites, start=1):
+        print_checking_results(index, site.strip())
+
